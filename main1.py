d306ff3c54f15c350e243ede7344f23007af4cda
MIbrahimcheema123 <iambestibrahim92@gmail.com>
2025-04-23 18:54:29 +0500

    initial commit

--- main1.py
+++ main1.py
@@ -1,15 +1,8 @@
 age = input("what is your age")
-
-print ("your age is",age)
-
 claas = input("in which class you are") 
-
-print ("you are in claas",claas)
-
 favouritefood = input("what is your favourite food")
-
-print ("your favourite food is",favouritefood)
-
 favouritesport = input("what is your favourite sport")
-
+print ("your age is",age)
+print ("you are in claas",claas)
+print ("your favourite food is",favouritefood)
 print ("your favourite sport is",favouritesport)
