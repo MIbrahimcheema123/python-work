09df71924313692a0e27d6105d37c7e084c2d1d8
MIbrahimcheema123 <iambestibrahim92@gmail.com>
2025-04-24 17:14:34 +0500

    initial commit

---
+++ py/typecasting.py
@@ -0,0 +1,17 @@
+name="10.5"
+age=13
+weight=40.5
+is_male=True
+print("type of name is:",type(name))
+print("type of age is:",type(age))
+print("type of weight is:",type(weight))
+print("type of is_male is:",type(is_male))
+print("\n after type casting")
+age=int(age)
+print(age)
+weight=int(weight)
+print(weight)
+name=float(name)
+print(name)
+is_male=bool(is_male)
+print(is_male)
