b8250ac8810061214d27aa86600d973165646bf7
MIbrahimcheema123 <iambestibrahim92@gmail.com>
2025-04-28 16:38:30 +0500

    initial commit

---
+++ main3.py
@@ -0,0 +1,11 @@
+ # height of trees
+tree1 = 90
+tree2 = 85
+tree3 = 100
+tree4 = 50
+tree5 = 27
+sum = tree1+tree2+tree3+tree4+tree5
+print("the sum of all tree height is",sum)
+# finding average of trees height
+average =sum/5
+print("average height of trees is",average)
