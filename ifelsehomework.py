d039fc37db5b25f383c0855dc891dd0d0f7f5c20
MIbrahimcheema123 <iambestibrahim92@gmail.com>
2025-05-02 17:01:40 +0500

    initial commit

---
+++ ifelsehomework.py
@@ -0,0 +1,12 @@
+# program to tell about the cloths you need to wear
+temperature = input("what is the temperature out today :")
+temperature = int(temperature)
+if temperature >= 30 :
+    print("wear light cloths")
+elif temperature >= 20 :
+    print("wear medium cloths")
+elif temperature < 20 :
+    print("wear warm cloths")
+
+# there is no need of else in this program 
+# hope so you like it
