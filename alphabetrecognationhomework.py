8d5e4eddc049083c62131d4eabaf4442cbd7e89e
MIbrahimcheema123 <iambestibrahim92@gmail.com>
2025-05-05 20:50:58 +0500

    initial commit

--- alphabetrecognationhomework.py
+++ alphabetrecognationhomework.py
@@ -2,4 +2,4 @@ character = input ("enter a alphabet :")
 if (character  > 'A' and character > 'Z') or (character  > 'a' and character > 'z') :
     print ("it is a alphabet")
 else :
-    print ("it is a symbol")
+    print ("it is a symbol or a number")
