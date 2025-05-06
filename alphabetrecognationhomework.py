66b48afee210d14ac6a2a1c92216e6ae6a440a4d
MIbrahimcheema123 <iambestibrahim92@gmail.com>
2025-05-06 22:12:27 +0500

    initial commit

--- alphabetrecognationhomework.py
+++ alphabetrecognationhomework.py
@@ -1,5 +1,8 @@
-character = input ("enter a alphabet :")
-if (character  > 'A' and character > 'Z') or (character  > 'a' and character > 'z') :
-    print ("it is a alphabet")
-else :
-    print ("it is a symbol or a number")
+# Program to tell if the character is a alphabet or a number or symbol
+# At first we will take input from the user
+# After that we will use if and else program to make the computer tell if it a alphabet,number,symbol
+character = input ("Enter a alphabet :")
+if (character > 'A' and character > 'Z') or (character  > 'a' and character > 'z') :
+    print ("It is a alphabet")
+else:
+    print ("It is a symbol or a number") 
